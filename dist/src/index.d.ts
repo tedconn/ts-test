2fe48be4fa228c0a9ccc9e4e88859172096c9eaf
Ted <tconn@salesforce.com>
2019-08-07 19:52:32 +0200

    added tsconfig.json

---
+++ dist/src/index.d.ts
@@ -0,0 +1,6 @@
+declare class Animal {
+    static type: string;
+    static run: () => void;
+}
+export { Animal };
+//# sourceMappingURL=index.d.ts.map
---
+++ dist/src/index.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class Animal {
+}
+Animal.type = "something";
+Animal.run = () => {
+    console.log("running all fast");
+};
+exports.Animal = Animal;
+//# sourceMappingURL=index.js.map
